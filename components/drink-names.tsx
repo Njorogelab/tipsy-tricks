fd830114432bd07867fdc8ba78e7013afae395cb
Kennedy Njoroge <njorogekennedy13@gmail.com>
2024-06-22 19:19:50 +1000

    drink name no cache

--- components/drink-names.tsx
+++ components/drink-names.tsx
@@ -1,29 +1,47 @@
-'use client'
-import { useState, useEffect } from 'react';
+'use client';
+
+import { useEffect, useState } from 'react';
+
+const drinkNames = [
+    'Lord of the Drinks',
+    'Tequila Mockingbird',
+    'Stranger Sips',
+    'Harry Pinter and the Order of the Phoenix',
+    'Gin of Thrones',
+    'The Shots-shank Redemption',
+    'The Whiskey of Oz',
+    'Booze to the Future',
+    'The Gincredibles',
+    'The Great Gin-sby',
+    'Orange is the New Black Label',
+    'Captain Morgan Phillips',
+    'Inglourious Blasters',
+    'The Bourbon Supremacy',
+    'The Drink Knight Rises',
+    'The Irishmanhattan',
+    'Mai Tai-nic',
+    'Alice in Wine-derland',
+    'Gindependence Day',
+    'Harry Potter and the Half-Pint Prince'
+];
 
 export function DrinkNames() {
-  const [drinkName, setDrinkName] = useState<string>();
+    const [drinkName, setDrinkName] = useState('');
 
-  useEffect(() => {
-    async function fetchDrinkName() {
-      try {
-        const response = await fetch(`/api/drinkname?timestamp=${new Date().getTime()}`, {cache: 'no-store'});
-        const data = await response.json();
-        console.log(data)
-        setDrinkName(data);
-      } catch (error) {
-        console.error('Error fetching random drink name:', error);
-      }
-    }
+    useEffect(() => {
+        function getRandomDrinkName() {
+            const randomIndex = Math.floor(Math.random() * drinkNames.length);
+            return drinkNames[randomIndex];
+        }
 
-    fetchDrinkName();
-  }, []);
+        setDrinkName(getRandomDrinkName());
+    }, []);
 
-  return (
-    <div className="items-center block w-full">
-      <p className="text-3xl font-extrabold text-center md:text-5xl lg:text-7xl">
-        {drinkName || 'Loading...'}
-      </p>
-    </div>
-  );
+    return (
+        <div className="items-center block w-full">
+            <p className="text-3xl font-extrabold text-center md:text-5xl lg:text-7xl">
+                {drinkName || "Loading..."}
+            </p>
+        </div>
+    );
 }
